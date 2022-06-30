bf166ce85fde37c0ba1649c7f680f78edefc68cb
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2022-06-30 12:49:01 +0200

    Upload dir intermediate file

--- pipeline.go
+++ pipeline.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-io/go-utils/ziputil"
 	"github.com/bitrise-steplib/steps-deploy-to-bitrise-io/uploaders"
 )
 
@@ -48,6 +51,7 @@ func parsePipelineIntermediateFiles(s string) (map[string]string, error) {
 
 type PipelineIntermediateFileMeta struct {
 	EnvKey string `json:"env_key"`
+	IsDir  bool   `json:"is_dir"`
 }
 
 func PushPipelineIntermediateFiles(fileList, buildURL, buildAPIToken string) error {
@@ -70,15 +74,35 @@ func PushPipelineIntermediateFiles(fileList, buildURL, buildAPIToken string) err
 			EnvKey: key,
 		}
 
+		fileInfo, err := os.Stat(pth)
+		if err != nil {
+			return err
+		}
+
+		if fileInfo.IsDir() {
+			tmpDir, err := pathutil.NormalizedOSTempDirPath("pipeline_file_share")
+			if err != nil {
+				return err
+			}
+
+			name := strings.TrimSuffix(filepath.Base(pth), filepath.Ext(pth))
+			targetPth := filepath.Join(tmpDir, name+".zip")
+
+			if err := ziputil.ZipDir(pth, targetPth, true); err != nil {
+				return fmt.Errorf("failed to zip output dir, error: %s", err)
+			}
+
+			pth = targetPth
+			meta.IsDir = true
+		}
+
 		b, err := json.Marshal(meta)
 		if err != nil {
 			return err
 		}
 
 		fakeMeta := map[string]interface{}{
-			"file_size_bytes": "",
-			"app_info":        string(b),
-			"scheme":          string(b),
+			"scheme": string(b),
 		}
 
 		_, err = uploaders.DeployFileWithMeta(pth, buildURL, buildAPIToken, "ios-xcarchive", fakeMeta)
