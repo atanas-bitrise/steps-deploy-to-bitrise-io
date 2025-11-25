7d2412a7eadde33a5beafcebb2641c24e34e21eb
Atanas Chanev <atanas.chanev@bitrise.io>
2025-11-25 17:49:42 +0000

    Testing for side effect

--- test/testasset/testasset.go
+++ test/testasset/testasset.go
@@ -13,12 +13,12 @@ var VideoTypes = []string{".mp4", ".webm", ".ogg"} // These video types are also
 func IsSupportedAssetType(fileName string) bool {
 	ext := filepath.Ext(fileName)
 
-	if slices.Contains(AssetTypes, strings.ToLower(ext)) {
+	if slices.Contains(AssetTypes, ext) {
 		return true
 	}
 
 	if os.Getenv("ENABLE_TEST_VIDEO_UPLOAD") == "true" {
-		return slices.Contains(VideoTypes, strings.ToLower(ext))
+		return slices.Contains(VideoTypes, ext)
 	}
 
 	return false
